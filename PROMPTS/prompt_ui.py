7b730c7efa8a1dd4253e9d6f4cd205baac8644e1
Anirudh G <anirudhgadekar44@gmail.com>
2026-06-04 19:11:37 +0530

    reusable template

--- PROMPTS/prompt_ui.py
+++ PROMPTS/prompt_ui.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
 
@@ -19,24 +19,7 @@ style_inputt = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "T
 length_inputt = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", 
 "Long (detailed explanation)"])
 
-#template
-template = PromptTemplate(
-    template= """
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-   - Include relevant mathematical equations if present in the paper.
-   - Explain the mathematical concepts using simple, intuitive code snippets where
-     applicable.
-2. Analogies:
-   - Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient
-information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables = ['paper_input','style_input','length_input']
-)
+template = load_prompt('template.json')
 
 # fill the placeholders
 prompt = template.invoke({
